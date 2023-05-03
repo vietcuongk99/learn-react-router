f210df810a1416c3e123e09eab2cbeeb89869be5
cuongdv78 <cuongdv78@ghtk.co>
2023-05-03 16:14:28 +0700

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
+import Contact, { loader as contactLoader } from "./routes/contact.jsx";
+import EditContact, { action as editAction } from "./routes/edit.jsx";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
